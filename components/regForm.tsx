63c97fb30bb8877b4df5d3e47493d37be254e61a
xNemiOx <tuman20041@yandex.ru>
2024-05-03 21:26:01 +0700

    api

--- components/regForm.tsx
+++ components/regForm.tsx
@@ -20,7 +20,7 @@ export default function Register() {
         }
     
         try {
-            const response = await fetch(`http://localhost:4000/api/auth/register`, {
+            const response = await fetch(`/api/auth/register`, {
                 method: 'POST',
                 body: JSON.stringify({
                     login: login,
